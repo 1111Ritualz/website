6c051f022d5a8177de9507cbc4f03b4686686d41
Audumbar-Patil <patil.audumbar.santosh@gmail.com>
2026-04-19 18:29:32 +0530

    deploy error blank screen

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    define: {
-      global: 'window',
-    },
-    base: command === 'serve' ? '/' : '/website/',
-  };
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: 'window',
+  },
+  base: './',   // ✅ THIS FIXES EVERYTHING
 })
